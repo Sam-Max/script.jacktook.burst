elgatito/script.elementum.burst

Fix ilcorsaronero (new design), cpasbien, megapeer, newstudio, torrent9. (#445)
Also improve queries for various tracker.

burst/burst.py
@@ -637,10 +637,10 @@ def extract_from_page(provider, content):
             log.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))
             return result
 
-        matches = re.findall(r'/download.php\?id=([A-Za-z0-9]{40})\W', content)
+        matches = re.findall(r'/download.php\?id=([A-Fa-f0-9]{40})\W', content)
         if matches:
             result = "magnet:?xt=urn:btih:" + matches[0]
-            log.debug('[%s] Matched download link: %s' % (provider, repr(result)))
+            log.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))
             return result
 
         matches = re.findall(r'/engine/download.php\?id=[A-Za-z0-9]+[^\s\'"]*', content) # animaunt
@@ -656,7 +656,7 @@ def extract_from_page(provider, content):
             log.debug('[%s] Matched download link: %s' % (provider, repr(result)))
             return result
 
-        matches = re.findall(r'/get_torrent/([A-Fa-f0-9]{40})', content)
+        matches = re.findall(r'/get_torrents?/([A-Fa-f0-9]{40})', content)
         if matches:
             result = "magnet:?xt=urn:btih:" + matches[0]
             log.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))

Sam-Max/script.jacktook.burst

burst/burst.py
@@ -624,10 +624,10 @@ def extract_from_page(provider, content):
             logging.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))
             return result
 
-        matches = re.findall(r'/download.php\?id=([A-Za-z0-9]{40})\W', content)
+        matches = re.findall(r'/download.php\?id=([A-Fa-f0-9]{40})\W', content)
         if matches:
             result = "magnet:?xt=urn:btih:" + matches[0]
-            logging.debug('[%s] Matched download link: %s' % (provider, repr(result)))
+            logging.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))
             return result
 
         matches = re.findall(r'/engine/download.php\?id=[A-Za-z0-9]+[^\s\'"]*', content) # animaunt
@@ -643,7 +643,7 @@ def extract_from_page(provider, content):
             logging.debug('[%s] Matched download link: %s' % (provider, repr(result)))
             return result
 
-        matches = re.findall(r'/get_torrent/([A-Fa-f0-9]{40})', content)
+        matches = re.findall(r'/get_torrents?/([A-Fa-f0-9]{40})', content)
         if matches:
             result = "magnet:?xt=urn:btih:" + matches[0]
             logging.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))