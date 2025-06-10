f6021f55a6aebfc8706e95ee1fc494c7600c4630
sammax <samagd@outlook.es>
2025-06-10 12:25:40 +0200

    refactor: streamline payload construction and result conversion in JacktookProvider

--- burst.py
+++ burst.py
@@ -9,113 +9,94 @@ sys.path.insert(
 )
 
 from burst.burst import search
-from jacktook.provider import Provider, ProviderResult
+from jacktook.provider_base import Provider, ProviderResult
 from burst.client import Client
 
 
-def general_payload(query):
-    return {
+def build_payload(**kwargs):
+    payload = {
         "silent": False,
         "skip_auth": False,
-        "query": query,
     }
+    payload.update(kwargs)
+    return payload
+
+
+def general_payload(query):
+    return build_payload(query=query)
 
 
 def movie_payload(id, title, titles, year):
-    return {
-        "silent": False,
-        "skip_auth": False,
-        "imdb_id": id,
-        "title": title,
-        "titles": titles,
-        "year": year,
-    }
+    return build_payload(imdb_id=id, title=title, titles=titles, year=year)
 
 
 def show_payload(id, title, titles, year):
-    return {
-        "silent": False,
-        "skip_auth": False,
-        "imdb_id": id,
-        "title": title,
-        "titles": titles,
-        "year": year,
-    }
+    return build_payload(imdb_id=id, title=title, titles=titles, year=year)
 
 
 def season_payload(id, title, season, titles, year):
-    return {
-        "silent": False,
-        "skip_auth": False,
-        "imdb_id": id,
-        "title": title,
-        "titles": titles,
-        "season": season,
-        "year": year,
-    }
+    return build_payload(
+        imdb_id=id, title=title, titles=titles, season=season, year=year
+    )
 
 
 def episode_payload(id, title, season, episode, titles, year):
-    return {
-        "silent": False,
-        "skip_auth": False,
-        "imdb_id": id,
-        "title": title,
-        "titles": titles,
-        "season": season,
-        "episode": episode,
-        "year": year,
-    }
+    return build_payload(
+        imdb_id=id,
+        title=title,
+        titles=titles,
+        season=season,
+        episode=episode,
+        year=year,
+    )
 
 
-def convert_results_jacktook(obj):
-    result = []
-    for item in obj:
-        result.append(
-            ProviderResult(
-                title=item["name"],
-                indexer=item["provider"],
-                guid=item["uri"],
-                quality="",
-                seeders=item["seeds"],
-                peers=item["peers"],
-                size=item["size"] if len(item["size"]) > 0 else 0,
-            )
+def to_provider_results(obj):
+    return [
+        ProviderResult(
+            title=item["name"],
+            indexer=item["provider"],
+            guid=item["uri"],
+            quality="",
+            seeders=item["seeds"],
+            peers=item["peers"],
+            size=item["size"] if len(item["size"]) > 0 else 0,
         )
-    return result
+        for item in obj
+    ]
 
 
 class JacktookProvider(Provider):
     def search(self, query):
         s = search(general_payload(query), "general")
-        s = convert_results_jacktook(s)
-        return s
+        return to_provider_results(s)
 
     def search_movie(self, id, title, titles="", year=""):
         logging.debug("search_movie")
         s = search(movie_payload(id, title, titles, year), "movie")
-        s = convert_results_jacktook(s)
-        logging.debug(s)
-        return s
+        results = to_provider_results(s)
+        logging.debug(results)
+        return results
 
     def search_show(self, id, title, titles="", year=""):
         logging.debug("search_show")
         s = search(show_payload(id, title, titles, year), "season")
-        s = convert_results_jacktook(s)
-        logging.debug(s)
-        return s
+        results = to_provider_results(s)
+        logging.debug(results)
+        return results
 
     def search_season(self, id, title, season_number, titles="", year=""):
         s = search(season_payload(id, title, season_number, titles, year), "season")
-        s = convert_results_jacktook(s)
-        return s
+        return to_provider_results(s)
 
-    def search_episode(self, id, title, season_number, episode_number, titles="", year=""):
+    def search_episode(
+        self, id, title, season_number, episode_number, titles="", year=""
+    ):
         s = search(
-            episode_payload(id, title, season_number, episode_number, titles, year), "episode"
+            episode_payload(id, title, season_number, episode_number, titles, year),
+            "episode",
         )
-        s = convert_results_jacktook(s)
-        return s
+        return to_provider_results(s)
 
     def resolve(self, provider_data):
         if isinstance(provider_data, dict):
