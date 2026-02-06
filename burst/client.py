elgatito/script.elementum.burst

Prefer user defined alias for tracker. (#470)
Domain name preferred order:
root_url -> base_url (see update_definitions()) -> public_dns_alias OR tor_dns_alias (see settings.xml) -> user defined alias

burst/client.py
@@ -113,7 +113,7 @@ def ResolvePublic(host):
 
 def ResolveOpennic(host):
     try:
-        log.debug("Custom DNS resolving with public DNS for: %s" % host)
+        log.debug("Custom DNS resolving with OpenNIC DNS for: %s" % host)
         resolver = dns.resolver.Resolver()
         resolver.nameservers = dns_opennic_list
         answer = resolver.query(host, 'A')

Sam-Max/script.jacktook.burst

burst/client.py
@@ -13,8 +13,10 @@ import os
 import urllib3
 import requests
 import antizapret
+
 try:
     import dns.resolver
+
     platform_can_resolve = True
 except:
     platform_can_resolve = False
@@ -120,7 +122,7 @@ def ResolvePublic(host):
 
 def ResolveOpennic(host):
     try:
-        logging.debug("Custom DNS resolving with public DNS for: %s" % host)
+        logging.debug("Custom DNS resolving with OpenNIC DNS for: %s" % host)
         resolver = dns.resolver.Resolver()
         resolver.nameservers = dns_opennic_list
         answer = resolver.query(host, "A")